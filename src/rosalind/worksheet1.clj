8d7e3690e8e33ede3b90a0af36849e552d905c35
Thomas Moerman <thomas.moerman@gmail.com>
2013-06-10 13:15:44 +0200

    revc solution

--- src/rosalind/worksheet1.clj
+++ src/rosalind/worksheet1.clj
@@ -1,36 +1,45 @@
 (ns rosalind.worksheet1)
 (use 'clojure.java.io)
+(use '[clojure.string :only (join split)])
+
+;;
+;; utility methods
+;;
+
+(defn lines [resource-file-name] (line-seq (reader (resource resource-file-name))))
+
+
 
 ;;
 ;; http://rosalind.info/problems/dna/
 ;;
 
-(def dna-file (resource "rosalind_dna.txt"))
+(def dna (first (lines "rosalind_dna.txt")))
 
-(def dna-lines (line-seq (reader dna-file)))
-
-(def dna (first dna-lines))
-
-(defn freqs [coll] (vals (sort (frequencies coll))))
+(defn freqs [collection] (vals (sort (frequencies collection))))
 
 (freqs dna)
 
+(println (freqs dna))
 
 
-;; table 
 
-(def proteins (apply hash-map '(a b c d)))
-
-(print proteins)
 
 ;;
 ;; http://rosalind.info/problems/revc/
 ;;
 
+(def revc (first (lines "rosalind_revc.txt")))
 
 (def dna-complement {\A \T \C \G \G \C \T \A})
 
-(def revc-temp "AAAACCCGGT")
+(defn reverse-complement [dna-string] (join (map dna-complement (reverse dna-string))))
+
+(reverse-complement revc)
+
+(println (reverse-complement revc))
 
 
-
+;;
+;; 
+;;
