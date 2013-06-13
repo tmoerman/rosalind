de0d7dfa92652badcb8de56aacdbdbea9b38a95f
Thomas Moerman <thomas.moerman@gmail.com>
2013-06-13 20:01:09 +0200

    removed main

--- project.clj
+++ project.clj
@@ -3,5 +3,4 @@
   :url "http://github.com/tmoerman/rosalind"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :main rosalind.core)
+  :dependencies [[org.clojure/clojure "1.5.1"]])
