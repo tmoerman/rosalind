23afeba3cf8e2f34fb1d9d3075ebab019934ecc3
Thomas Moerman <thomas.moerman@gmail.com>
2013-06-18 12:44:36 +0200

    added perm solution

---
+++ src/rosalind/perm.clj
@@ -0,0 +1,31 @@
+(ns rosalind.perm
+  (:use clojure.java.io)
+  (:use [clojure.string :only (join)]))
+
+
+;;
+;; http://rosalind.info/problems/perm/
+;;
+
+(defn permutate [coll]
+  (if (empty? coll)
+    '(nil) ;; remember List(Nil) from the Scala Coursera course
+    (for [e  coll
+          :let [tail (filter (complement (partial = e)) coll)]
+          c (permutate tail)]
+      (cons e c))))
+
+(defn make-range [n] 
+  (range 1 (inc n)))
+
+(permutate (make-range 3))
+
+(defn perm-result [n]
+  (let [perms (permutate (make-range n))]
+    (spit "resources/rosalind_perm_out.txt"
+      (str (count perms) "\n"
+        (join "\n" (map #(join " " %) perms))))))
+
+(def n 3)
+
+(perm-result n)
