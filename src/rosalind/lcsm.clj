9b27fbc43e5128fb1edc57b620869978882a63f6
Thomas Moerman <thomas.moerman@gmail.com>
2013-06-20 12:07:48 +0200

    added lcsm solution

--- src/rosalind/lcsm.clj
+++ src/rosalind/lcsm.clj
@@ -1,4 +1,43 @@
 (ns rosalind.lcsm
-  (:use clojure.java.io))
+  (:use clojure.java.io)
+  (:use rosalind.fasta))
 
-(prn "hello world")
+;;
+;; http://rosalind.info/problems/lcsm/
+;;
+
+(defn only-fasta-seqs-as-strings 
+  [fasta-map]
+  (->> fasta-map
+       (map :seq)
+       (map (partial apply str))))
+
+(defn partition-sliding [n s] 
+  (->> s 
+       (partition n 1)
+       (map (partial apply str))))
+
+(defn shortest [s1 s2] (if (< (count s1) (count s2)) s1 s2))
+
+(defn contained-in-all? [all s] (every? #(.contains % s) all))
+
+(defn find-shared-motif [dna-strings]
+  (let [ref-dna  (reduce shortest dna-strings)
+        rest-dna (filter (partial not= ref-dna) dna-strings)]
+    
+    (loop [length (count ref-dna)]
+      (print '.) ;; remove me
+      (if-let [result (first (filter (partial contained-in-all? rest-dna) (partition-sliding length ref-dna)))]
+        result
+        (recur (dec length))))))
+
+(prn
+  (->> "rosalind_lcsm.txt"
+       (resource)
+       (reader)
+       (line-seq)
+       (parse-fasta)
+       (only-fasta-seqs-as-strings)
+       (find-shared-motif)))
+
+;(just-do-it)
