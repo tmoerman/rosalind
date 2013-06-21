44ab0ff96b9be2537774360bf8c228d8096133d5
Thomas Moerman <thomas.moerman@gmail.com>
2013-06-21 12:35:18 +0200

    added fns file

---
+++ src/scraps/fns.clj
@@ -0,0 +1,17 @@
+(ns scraps.fns)
+
+;;
+;; Applying a list of functions to an argument
+;;
+
+(defn translate [x] (+ x 1))
+(defn square [x] (* x x))
+(defn double [x] (+ x x))
+
+(def fns [translate square double])
+
+;; optie 1
+(map #(% 2) fns)
+
+;; optie 2
+((apply juxt fns) 2)
