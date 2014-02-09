38b873c73eeaa704b07805f1165e542d86da7e47
Thomas Moerman <thomas.moerman@gmail.com>
2014-02-09 10:36:35 +0100

    refactored with threading macro

--- src/rosalind/dna.clj
+++ src/rosalind/dna.clj
@@ -1,13 +1,15 @@
 (ns rosalind.dna
-  (:use clojure.java.io)
-  (:use [clojure.string :only (trim)]))
+  (require [clojure.java.io :as io]
+           [clojure.string :as str]))
 
 ;;
 ;; http://rosalind.info/problems/dna/
 ;;
 
-(def dna-data (trim (slurp (resource "rosalind_dna.txt"))))
-
-(defn freqs [collection] (vals (sort (frequencies collection))))
-
-(freqs dna-data)
+(->> "rosalind_dna.txt"
+  (io/resource)
+  (slurp)
+  (str/trim)
+  (frequencies)
+  (sort)
+  (vals))
