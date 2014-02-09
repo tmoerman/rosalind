3b6e62b5f94c0f152ae95f7403a9131ad1b2000e
Thomas Moerman <thomas.moerman@gmail.com>
2014-02-09 11:00:13 +0100

    refactored

--- src/rosalind/lcsm.clj
+++ src/rosalind/lcsm.clj
@@ -1,19 +1,19 @@
 (ns rosalind.lcsm
-  (:use clojure.java.io)
-  (:use rosalind.fasta))
+  (require [clojure.java.io :as io]
+           [rosalind.fasta :as fasta]))
 
 ;;
 ;; http://rosalind.info/problems/lcsm/
 ;;
 
-(defn only-fasta-seqs-as-strings 
+(defn only-fasta-seqs-as-strings
   [fasta-map]
   (->> fasta-map
        (map :seq)
        (map (partial apply str))))
 
-(defn partition-sliding [n s] 
-  (->> s 
+(defn partition-sliding [n s]
+  (->> s
        (partition n 1)
        (map (partial apply str))))
 
@@ -26,7 +26,7 @@
 (defn find-shared-motif [dna-strings]
   (let [ref-dna  (reduce shortest dna-strings)
         rest-dna (filter (partial not= ref-dna) dna-strings)]
-    
+
     (loop [length (count ref-dna)]
 
       (if-let [result (first (filter (partial contained-in-all? rest-dna) (partition-sliding length ref-dna)))]
@@ -38,7 +38,7 @@
 (defn binary-find-longest-common-substring-length [dna-strings]
   (let [ref-dna  (reduce shortest dna-strings)
         rest-dna (filter (partial not= ref-dna) dna-strings)]
-    
+
     (loop [l 0
            r (count ref-dna)]
       (if (= (inc l) r)
@@ -56,10 +56,10 @@
 ;; combinator
 
 (->> "rosalind_lcsm.txt"
-     (resource)
-     (reader)
+     (io/resource)
+     (io/reader)
      (line-seq)
-     (parse-fasta)
+     (fasta/parse-fasta)
      (only-fasta-seqs-as-strings)
      (find-shared-motif-fast)
      (prn))
