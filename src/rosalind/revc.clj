16c5f3d8a3d0df1fb43e21045b7ac07e0ec483f0
Thomas Moerman <thomas.moerman@gmail.com>
2014-02-09 11:32:13 +0100

    refactored

--- src/rosalind/revc.clj
+++ src/rosalind/revc.clj
@@ -1,14 +1,16 @@
 (ns rosalind.revc
-  (:use clojure.java.io)
-  (:use [clojure.string :only (trim join)])
-  (:require [rosalind.core]))
+  (require [clojure.java.io :as io]
+           [rosalind.core :as ros]
+           [clojure.string :as str]))
 
 ;;
 ;; http://rosalind.info/problems/revc/
 ;;
 
-(def revc-data (trim (slurp (resource "rosalind_revc.txt"))))
-
-(defn reverse-complement [dna-string] (join (map dna-complement (reverse dna-string))))
-
-(reverse-complement revc-data)
+(->> "rosalind_revc.txt"
+  (io/resource)
+  (slurp)
+  (str/trim)
+  (reverse)
+  (ros/dna-complement)
+  (str/join))
