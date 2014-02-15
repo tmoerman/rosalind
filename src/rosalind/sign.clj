857215f48a1a50b6733559f84a5f443aa9a31d45
Thomas Moerman <thomas.moerman@gmail.com>
2014-02-16 00:09:25 +0100

    refactored

--- src/rosalind/sign.clj
+++ src/rosalind/sign.clj
@@ -11,14 +11,14 @@
 (defn signed-permutations [n]
   (for [selection   (combo/selections [-1 1] n)
         permutation (combo/permutations (range++ n))]
-    (map vector selection permutation)))
+    (map (partial reduce *)
+         (map vector selection permutation))))
 
 (defn output [list]
   (str (count list) "\n" (str/join list)))
 
 (->>
   (signed-permutations 4)
-  (map (partial map (partial reduce *)))
   (map (partial str/join " "))
   (map #(str % "\n"))
   (output)
